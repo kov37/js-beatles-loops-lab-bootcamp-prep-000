c27c9ee78f337808db2a38d31e9efa0ac69b933f
kovner chalumo <kchalumo@gmail.com>
2018-12-26 01:09:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// add solution here
+function theBeatlesPlay(array, instruments){
+  arr = [];
+  for (var i= 0; i < array.length; i++){
+    arr.push(`${array[i]} plays ${instruments[i]}`);
+  }
+  return arr;
+}
+
+function johnLennonFacts(array){
+  
+}
